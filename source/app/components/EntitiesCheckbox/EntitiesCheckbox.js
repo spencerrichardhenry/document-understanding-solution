9724cee1b5166b47990780ca3cb7c8f81f6c0051
Shivani Mehendarge <smehenda@amazon.com>
2021-02-08 15:37:12 -0800

    add parameter for comprehend medical for users to opt in only if needed

--- source/app/components/EntitiesCheckbox/EntitiesCheckbox.js
+++ source/app/components/EntitiesCheckbox/EntitiesCheckbox.js
@@ -30,6 +30,7 @@ import {
 
 
 import css from './EntitiesCheckbox.scss'
+import { ENABLE_COMPREHEND_MEDICAL } from '../../constants/configs'
 
 export default function EntitiesCheckbox({
   entities,
@@ -60,7 +61,7 @@ export default function EntitiesCheckbox({
   let is_comprehend_medical = false
   
 
-  if (comprehendService == COMPREHEND_MEDICAL_SERVICE)is_comprehend_medical=true
+  if (comprehendService == COMPREHEND_MEDICAL_SERVICE && ENABLE_COMPREHEND_MEDICAL)is_comprehend_medical=true
 
   
   if (!entities.length&& visible) {
